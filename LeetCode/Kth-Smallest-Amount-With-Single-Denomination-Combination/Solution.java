a6333deba0bf9acffbc78c9a51f7e2009f45b38b
Jhushiketh Gaddam <gjhushiketh@gmail.com>
2026-08-21 12:48:45 +0530

    Add solution for Kth Smallest Amount With Single Denomination Combination

---
+++ LeetCode/Kth-Smallest-Amount-With-Single-Denomination-Combination/Solution.java
@@ -0,0 +1,9 @@
+def findKthSmallest(self, A: List[int], k: int) -> int:
+    # A.sort() + filter multiples
+    def check(m):
+        tot = 0
+        for x in range(1, len(coins) + 1):
+            for c in combinations(coins, x):
+                tot += m // lcm(*c) * pow(-1, x + 1)
+        return tot >= k
+    return bisect_left(range(k * coins[0] + 1), True, lo=1, key=check)
